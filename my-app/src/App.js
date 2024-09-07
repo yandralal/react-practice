19698e4842914bd09d18788e498b27c95bd04d26
yandralal <yandralal@live.com>
2024-09-07 18:57:15 +0530

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Alert from './components/Alert';
+import About from './components/About';
 import TextForm from './components/TextForm';
 import {
   BrowserRouter as Router,
@@ -44,7 +45,7 @@ function App() {
         <Alert alert={alert}/>
         <div className="container my-3">
           <Routes>
-            <Route exact path="/about" element={<About />} />
+            <Route exact path="/about" element={<About mode={mode} />} />
             <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" showAlert={showAlert} />} />
           </Routes>
         </div>
--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About(props) {
 
     let myStyle = {
-        color: props.mode === 'dark' ? 'white' : 'black',
+        color: props.mode === 'dark' ? 'white' : 'rgb(36 74 104)',
         backgroundColor: props.mode === 'dark' ? 'rgb(36 74 104)' : 'white',
     }
 
